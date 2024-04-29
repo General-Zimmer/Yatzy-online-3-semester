4afbd5681d1e2accf4301e646addcf1945c367d8
GZimmer <kristianz1000@gmail.com>
2024-04-29 13:46:37 +0200

    Added some empty posts and gets

--- api/Zimmers-amazing-API.js
+++ api/Zimmers-amazing-API.js
@@ -2,3 +2,44 @@ import {router} from 'express';
 
 const api = router();
 
+api.get('/', (request, response) => {
+    response.status(200).send({message: 'Hello, world!'});
+});
+
+
+api.post('/throw', async (request, response) => {
+
+});
+
+api.post('/lockdice', async (request, response) => {
+
+});
+
+api.post('/lockdices', async (request, response) => {
+
+});
+
+api.post('/lockdice', async (request, response) => {
+
+});
+
+api.post('/restartgame', async (request, response) => {
+
+});
+
+api.post('/nextturn', async (request, response) => {
+
+});
+
+api.get('/dices', async (request, response) => {
+
+});
+
+api.get('/scores', async (request, response) => {
+
+});
+
+// ZIMMER, REMEMBER DIS ALWAYS LAST. (You know why >:D)
+let exports = {api};
+export default exports; 
+export {api};
