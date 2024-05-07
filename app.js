ffa8a0a6d61400ba3baa83865411991b016b2804
Secondo92 <secondo92@hotmail.com>
2024-05-07 15:47:16 +0200

    prøver at få session til at virke

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ import session from 'express-session';
 import playersRouter from './api/api.js';
 import gameRouter from './api/gameLogic.js';
 
+
 const app = express();
+// Middleware der dirigerer anmodninger til vores "router" RESTful api
+app.use('/api/players', playersRouter);
+app.use('/gameLogic', gameRouter);
 
 app.use(express.static('assets'));
 app.use(express.json());
@@ -11,41 +15,50 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug');
 
-
 // Konfiguration af session middleware
 app.use(session({
     secret: "Secret_Sauce",
     resave: false,
-    saveUninitialized: true,
-
+    saveUninitialized: true
 }));
-
 
 // Loader pug startsiden
 app.get('/', (request, response) => {
-    response.render('login');
+    response.render('login', {title: "Welcome to yahtzeeeeeeee", knownUser: request.session.isLoggedIn});
 });
-
 
 // HTTP request for at gemme brugernavn, fra request,
 // i session og omdirigerer brugeren til yatzyspillet
-app.post('/', (request, response) => {
-    const user = request.body.username;
-    request.session.username = user;
-    console.log(`Player session created: ${user}`)
-    response.redirect('/yatzy')
-});
+app.post('/', async (request, response) => {
+    console.log("Session count: " + request.session.sessionCount);
 
+    const user = request.body.username;
+    if(!request.session.isLoggedIn){
+        request.session.username = user;
+        request.session.sessionCount = (request.session.sessionCount || 0) + 1;
+        console.log(`Player session created: ${user}`);
+        request.session.isLoggedIn = true;
+    }
+
+    console.log("Session count: " + request.session.sessionCount);
+    response.redirect('/yatzy');
+});
 
 // Render yatzy pug
 app.get('/yatzy', (request, response) =>{
-    response.render('yatzy')
-})
-
-
-// Middleware der dirigerer anmodninger til vores "router" RESTful api
-app.use('/api/players', playersRouter);
-app.use('/gameLogic', gameRouter);
+    response.render('yatzy');
+});
+    
+app.get('/logout', (request, response) => {
+    request.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            response.redirect('/');
+        }
+    });
+}
+);
 
 app.listen(8000, () => {
     console.log("Server running on port 8000");
