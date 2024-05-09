34b06b74966420436d2fa8b5f14a486d50c01ad9
Secondo92 <secondo92@hotmail.com>
2024-05-09 21:14:50 +0200

    Fixed navn på variabel

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get('/ayo', (request, response) => {
 // HTTP request for at hente brugernavn fra request body og lave en ny session.
 // Klient redirected til spillet hvis der er plads til en ny spiller. Hvis ikke, bliver klienten redirected til lobby
 app.post('/', async (request, response) => {
-    const user = request.body.username;
+    const user = request.body.Spiller;
     if(!request.session.isLoggedIn){
         request.session.username = user;
 
